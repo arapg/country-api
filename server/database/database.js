a7348427e0cec41e283dcc931af15eefc2434eb9
arpg <araceli.prietogallego@yh.nackademin.se>
2023-03-02 15:52:27 +0100

    Uploaded changes

---
+++ server/database/database.js
@@ -0,0 +1,5 @@
+module.exports.countries = {
+    'sweden': { population: 10000000, capital: 'Stockholm', language: 'Swedish' },
+    'norway': { population: 5000000, capital: 'Oslo', language: 'Norwegian' },
+    'denmark': { population: 6000000, capital: 'Copenhagen', language: 'Danish' }
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,63 +1,78 @@
 const express = require('express');
 const server = express();
-//const { countries } = require('../database/database.js');
+const { countries } = require('../database/database.js');
+require('dotenv').config();
 
-let countries = [
-    {
-      name: 'Sweden',
-      population: 1000000,
-      capital: 'CapitalA',
-      language: 'LanguageA'
-    },
-    {
-      name: 'CountryB',
-      population: 5000000,
-      capital: 'CapitalB',
-      language: 'LanguageB'
-    }
-  ]
+const securityCode = process.env.SECURITY_CODE;
 
-  server.get('/country/name=Sweden', (req, res) => {
-    const { name } = req.params;
-  
+server.use(express.json());
+
+
+server.get('/countries', (req, res) => {
+  res.send(countries);
+})
+
+server.get('/country/:name', (req, res) => {
+  const { name } = req.params;
+
+  if (!countries[name]) {
+    return res.status(404).send('Country not found');
+  } else {
+      res.send(countries[name]);
+  }
+});
+
+server.post('/add/:security', (req, res, next) => {
+    const { security } = req.params;
+
+    if( security === securityCode) {
+
+      if(Object.keys(req.body).length !== 4) {
+        return res.status(400).send('Expected 4 keys, received too many or too few');
+      } else {
+        const { name, population, capital, language } = req.body;
+        countries[name] = { population, capital, language};
+        
+        res.status(201).send('Added country.');
+      };
+    } else {
+      res.status(401).send('You are not authorized')
+    };
+});
+
+server.patch('/edit/:security/:name', (req, res) => {
+  const { security } = req.params;
+  const { name } = req.params;
+
+  if( security === securityCode) {
     if (!countries[name]) {
       return res.status(404).send('Country not found');
     } else {
-        res.send(countries[name]);
-    }
+      if(Object.keys(req.body).length !== 4) {
+        return res.status(400).send('Expected 4 keys, received too many or too few');
+      } else {
+        const { name, population, capital, language } = req.body;
+        countries[name] = { population, capital, language};
 
-  });
-
- server.use(myMiddleware);
- server.use(verifySecretCode);
-
-server.post('/add', (req, res, next) => {
-    const country = req.body;
-    countries.push(country);
-    res.status(201).send('Added country.');
+        res.status(200).send('Edited country');
+      };
+    };
+  };
 });
 
-function verifySecretCode(req, res, next) {
-    const secretCode = req.get('X-Secret-Code');
-    if (secretCode === 'mySecretCode') {
-      next(); // continue to the next middleware or route handler
-    } else {
-      res.status(401).send('Unauthorized'); // send a 401 Unauthorized response
-    }
-  };
+server.delete('/del/:security/:name', (req, res) => {
+  const { security } = req.params;
+  const { name } = req.params;
 
-  function myMiddleware(req, res, next) {
-    if(req.headers['content-type'] !== 'application/json') {
-        console.log('The request body was not of type JSON');
-        next();
-        return;
-    }
-    req.on('data', (chunk) => {
-        const readableChunk = chunk.toString();
-        const body = JSON.parse(readableChunk);
-        req.body = body;
-        next();
-    })
-}
+  if( security === securityCode) {
+    if (!countries[name]) {
+      return res.status(404).send('Country not found');
+    } else {
+      delete countries[name];
+      res.status(200).send('Deleted country');
+    };
+  };
+})
+
 
 server.listen(5050);
