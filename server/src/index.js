a1c007471381b1032f00995274f1a3f09737945a
arpg <araceli.prietogallego@yh.nackademin.se>
2023-03-06 10:54:46 +0100

    added validation with joi

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const Joi = require('joi');
 const server = express();
 const { countries } = require('../database/database.js');
-require('dotenv').config();
-
+require('dotenv').config({path:'../../.env'})
 const securityCode = process.env.SECURITY_CODE;
+
+const countrySchema = Joi.object({
+  name: Joi.string().required(),
+  population: Joi.number().integer().required(),
+  capital : Joi.string().required(),
+  language: Joi.string().required()
+});
 
 server.use(express.json());
 
@@ -17,46 +24,45 @@ server.get('/country/:name', (req, res) => {
 
   if (!countries[name]) {
     return res.status(404).send('Country not found');
-  } else {
-      res.send(countries[name]);
   }
+    res.send(countries[name]);
 });
 
-server.post('/add/:security', (req, res, next) => {
+server.post('/add/:security', (req, res) => {
     const { security } = req.params;
+    const { error, value } = countrySchema.validate(req.body);
 
     if( security === securityCode) {
 
-      if(Object.keys(req.body).length !== 4) {
-        return res.status(400).send('Expected 4 keys, received too many or too few');
-      } else {
-        const { name, population, capital, language } = req.body;
-        countries[name] = { population, capital, language};
-        
-        res.status(201).send('Added country.');
-      };
-    } else {
-      res.status(401).send('You are not authorized')
-    };
+      if(error) {
+        return res.status(400).send(error.details);
+      }
+      const { name, population, capital, language } = req.body;
+      countries[name] = { population, capital, language};
+      return res.status(201).send('Added country');
+    }
+
+      res.status(401).send('You are not authorized');
 });
 
-server.patch('/edit/:security/:name', (req, res) => {
+server.patch('/edit/:security/:country', (req, res) => {
   const { security } = req.params;
-  const { name } = req.params;
+  const { country } = req.params;
+  const { error, value } = countrySchema.validate(req.body);
 
   if( security === securityCode) {
-    if (!countries[name]) {
+    if (!countries[country]) {
       return res.status(404).send('Country not found');
-    } else {
-      if(Object.keys(req.body).length !== 4) {
-        return res.status(400).send('Expected 4 keys, received too many or too few');
-      } else {
-        const { name, population, capital, language } = req.body;
-        countries[name] = { population, capital, language};
+    } 
+    
+    if(error) {
+      return res.status(400).send(error.details);
+    }
 
-        res.status(200).send('Edited country');
-      };
-    };
+    const { name, population, capital, language } = req.body;
+    countries[name] = { population, capital, language};
+    return res.status(200).send('Edited country');
+    
   };
 });
 
@@ -67,10 +73,9 @@ server.delete('/del/:security/:name', (req, res) => {
   if( security === securityCode) {
     if (!countries[name]) {
       return res.status(404).send('Country not found');
-    } else {
-      delete countries[name];
-      res.status(200).send('Deleted country');
     };
+    delete countries[name];
+    res.status(200).send('Deleted country');
   };
 })
 
